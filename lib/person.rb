d75e47a78ef98107e7e38f9ea688a66f503919d2
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-25 16:21:55 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,4 @@
 class Person
 attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
-  #your code here
+avi_attributes = { name: , hair_color: , eye_color:  , height:}  #your code here
 end

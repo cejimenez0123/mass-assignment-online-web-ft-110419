30bb724b383b3ae58008f144f51a67d8d9da3da4
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-25 16:23:06 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,7 @@
 class Person
 attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
-avi_attributes = { name: , hair_color: , eye_color:  , height:}  #your code here
+  #your code here
+  def initialize(attributes)
+  end
+    
 end
